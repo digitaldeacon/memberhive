1f6bd421040c8b17392a86c0c565f8dc01be16e4
axtho <thomas.hochstetter@me.com>
2017-02-01 12:32:30 +0100

    person edit: start with image cropper

--- web/app/person/view/person-view.component.ts
+++ web/app/person/view/person-view.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {TitleService} from "../../common/title.service";
 import {ActivatedRoute, Params} from "@angular/router";
 import {PersonService} from "../person.service";
 import {Person} from "../person";
+import {ImageCropperComponent, CropperSettings, Bounds} from 'ng2-img-cropper';
 
 @Component({
     moduleId: 'mh-person',
@@ -12,9 +13,35 @@ import {Person} from "../person";
 })
 export class PersonViewComponent implements OnInit {
     private person: Person;
+    data:any;
+    cropperSettings:CropperSettings;
+    @ViewChild('cropper', undefined) cropper:ImageCropperComponent;
+
     constructor(private titleService: TitleService,
                 private route: ActivatedRoute,
                 private personService: PersonService) {
+        this.cropperSettings = new CropperSettings();
+        this.cropperSettings.width = 200;
+        this.cropperSettings.height = 200;
+        this.cropperSettings.keepAspect = false;
+
+        this.cropperSettings.croppedWidth = 200;
+        this.cropperSettings.croppedHeight = 200;
+
+        this.cropperSettings.canvasWidth = 500;
+        this.cropperSettings.canvasHeight = 300;
+
+        this.cropperSettings.minWidth = 100;
+        this.cropperSettings.minHeight = 100;
+
+        this.cropperSettings.rounded = true;
+        this.cropperSettings.minWithRelativeToResolution = false;
+
+        this.cropperSettings.cropperDrawSettings.strokeColor = 'rgba(255,255,255,1)';
+        this.cropperSettings.cropperDrawSettings.strokeWidth = 2;
+        this.cropperSettings.noFileInput = true;
+
+        this.data = {};
     }
 
     ngOnInit(): void {
@@ -24,5 +51,19 @@ export class PersonViewComponent implements OnInit {
                 this.person = person;
                 this.titleService.setTitle('Person: ' + person.fullName);
         });
+    }
+
+    fileChangeListener($event) {
+        var image:any = new Image();
+        var file:File = $event.target.files[0];
+        var myReader:FileReader = new FileReader();
+        var that = this;
+        myReader.onloadend = function (loadEvent:any) {
+            image.src = loadEvent.target.result;
+            that.cropper.setImage(image);
+
+        };
+
+        myReader.readAsDataURL(file);
     }
 }
