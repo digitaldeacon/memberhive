b6e376f1e1cd775099759a4b06dd64436098eb7c
axtho <thomas.hochstetter@me.com>
2017-02-16 11:50:04 +0100

    sidenav styling

--- web/app/view.component.ts
+++ web/app/view.component.ts
@@ -11,11 +11,10 @@ import {TitleService} from './common/title.service';
                 width: '200px'
             })),
             state('true',  style({
-                width: '50px',
+                width: '60px',
                 flex: '1 1 50px;',
-                'min-width': '50px',
-                'max-width': '50px',
-                'margin-left': '5px'
+                'min-width': '60px',
+                'max-width': '60px'
             }))
             /*transition('0 => 1', animate('200ms ease-in')),
             transition('1 => 0', animate('200ms ease-out'))*/
