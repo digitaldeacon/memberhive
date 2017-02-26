6207e59c912abb3c57a8a2b1a617b6fb757d5056
axtho <thomas.hochstetter@me.com>
2017-02-27 00:45:37 +0100

    #36 mail style init

---
+++ api/mail/layouts/html.php
@@ -0,0 +1,28 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this \yii\web\View view component instance */
+/* @var $message \yii\mail\MessageInterface the message being composed */
+/* @var $content string main view render result */
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>" />
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+</head>
+<body>
+<?php $this->beginBody() ?>
+<?= $content ?>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
---
+++ api/mail/newpw.php
@@ -0,0 +1,16 @@
+<div class="demo-card-wide mdl-card mdl-shadow--2dp">
+    <div class="mdl-card__title">
+        <h2 class="mdl-card__title-text">Hello <?=$name?></h2>
+    </div>
+    <div class="mdl-card__supporting-text">
+        <p>
+            Your user is: <?=$username?>
+        </p>
+        <p>
+            Your password: <?=$password?>
+        </p>
+    </div>
+    <div class="mdl-card__actions mdl-card--border">
+        Your MemberHive Team
+    </div>
+</div>
