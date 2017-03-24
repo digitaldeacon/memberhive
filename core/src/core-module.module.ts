dca041d190baff4f297edb0976e27db8dba39fa0
axtho <thomas.hochstetter@me.com>
2017-03-24 16:00:21 +0100

    #64 initial

---
+++ core/src/core-module.module.ts
@@ -0,0 +1,20 @@
+import { Observable } from 'rxjs';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+    declarations: [
+
+    ],
+    providers: [  ],
+    exports: [
+
+    ],
+    imports: [
+        BrowserModule
+    ],
+    schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+    ]
+})
+export class CoreModule {}
