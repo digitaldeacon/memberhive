3a1c14284732909cd48c3d2a1cdf339e086780fb
axtho <thomas.hochstetter@me.com>
2017-05-20 14:40:35 +0200

    add missing rxjs operator

--- core/src/modules/auth/auth.effects.ts
+++ core/src/modules/auth/auth.effects.ts
@@ -2,6 +2,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/of';
 import { defer } from 'rxjs/observable/defer';
 import { Injectable } from '@angular/core';
 import { Response } from '@angular/http';
--- core/src/modules/settings/settings.effects.ts
+++ core/src/modules/settings/settings.effects.ts
@@ -4,6 +4,7 @@ import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/toArray';
+import 'rxjs/add/observable/of';
 import { Injectable } from '@angular/core';
 import { Action } from '@ngrx/store';
 import { Effect, Actions, toPayload } from '@ngrx/effects';
