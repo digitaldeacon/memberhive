218129f2f17ffea2c2b486523363a0ba03744e7e
axtho <thomas.hochstetter@me.com>
2017-06-27 15:40:21 +0200

    fix minor issues

--- web/src/app/person/dialogs/map/map.dialog.ts
+++ web/src/app/person/dialogs/map/map.dialog.ts
@@ -11,7 +11,7 @@ export class MapDialogComponent implements OnInit {
 
     initMarker: GeoMarker;
     markers: GeoMarker[] = [];
-    zoom: number = 11;
+    zoom: number = 13;
     initMarkerToMap: boolean = false;
 
     constructor(
