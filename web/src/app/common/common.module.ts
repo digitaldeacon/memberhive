5d9a10b80bd99950c9a880c3f3f2c2f2a0ad94d7
axtho <thomas.hochstetter@me.com>
2017-10-06 13:34:11 +0200

    linting

--- web/src/app/common/common.module.ts
+++ web/src/app/common/common.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule} from "@angular/common";
+import { CommonModule } from '@angular/common';
 import { Title }  from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {
