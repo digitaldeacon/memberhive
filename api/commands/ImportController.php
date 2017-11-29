8e37073848004b4d8ae05e84cad11c4a03dce3f8
axtho <thomas.hochstetter@me.com>
2017-11-29 12:42:14 +0100

    extend import to families

--- api/commands/ImportController.php
+++ api/commands/ImportController.php
@@ -1,8 +1,10 @@
 <?php
 namespace app\commands;
 
+use app\models\Family;
 use app\models\Import;
 use app\models\Person;
+use app\models\PersonFamily;
 use yii\console\Controller;
 use Elvanto_API;
 
@@ -79,7 +81,7 @@ class ImportController extends Controller
             $person->phoneHome = $item->phone;
             $person->phoneMobile = $item->mobile;
 
-            // addreses
+            // addresses
             $person->address = json_encode(
                 [
                 'home' => [
@@ -91,14 +93,17 @@ class ImportController extends Controller
                 ]
             );
 
+            //family
+
             if (!$person->save()) {
                 var_dump($person->getErrors());
                 return 1;
             }
-
             //$this->logImport('elvanto', $item->id, $person);
         }
         echo "Successfully inserted $inserted and updated $updated Persons\n";
+
+        $this->setFamilies($authDetails);
         return 0;
     }
 
@@ -121,6 +126,100 @@ class ImportController extends Controller
             ];
         }
         return [];
+    }
+
+    private function setFamilies($authDetails)
+    {
+        $elvanto = new Elvanto_API($authDetails);
+
+        // see https://www.elvanto.com/api/people-fields/, Optional Fields
+        $extraFields = [
+            'family'
+        ];
+
+        $results = $elvanto->call('people/getAll', ['fields'=>$extraFields]);
+        $families = [];
+        foreach ($results->people->person as $item) {
+            if (!empty($item->family)) {
+                $id = $item->family->family_id;
+                $fname = '';
+                $families[$id] = [
+                    'name' => '',
+                    'adr' => '',
+                    'members' => []
+                ];
+                foreach ($item->family->family_member as $member) {
+                    $person = Person::findOne(['firstName'=>$member->firstname,'lastName'=>$member->lastname]);
+                    if ($member->relationship == 'Primary Contact') {
+                        $fname = $member->lastname . ' (' . $member->firstname . ')';
+                        $families[$id]['name'] = $fname;
+                        $families[$id]['adr'] = trim($person->address);
+                    }
+                    if (!empty($person)) {
+                        $fmember = [
+                            'id' =>$person->id,
+                            'role' => $this->setRole($member->relationship, $person)
+                        ];
+                        array_push($families[$id]['members'], $fmember);
+                    }
+                }
+            }
+        }
+
+        foreach ($families as $family) {
+            $fam = Family::findOne(['name'=>$family['name']]);
+            if (!$fam) {
+                $fam = new Family(['name'=>$family['name']]);
+                $fam->save();
+            }
+            if (!empty($family['members'])) {
+                foreach($family['members'] as $fmember) {
+                    $person = Person::findOne(intval($fmember['id']));
+                    if (!empty($person)) {
+                        $pfam = new PersonFamily([
+                            'person_id'=>$person->id,
+                            'family_id'=>$fam->id
+                        ]);
+                        $pfam->role = $fmember['role'];
+                        $pfam->is_primary = $family['adr'] == trim($person->address);
+                        if(!$pfam->save()) {
+                            echo json_encode($pfam->getErrors());
+                        } else {
+                            var_dump( 'Added '.$person->fullName.' to family '.$family['name']
+                                .' with role '.$fmember['role']. ' ('.$pfam->is_primary.')');
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private function isRolePrimary($role, $person) {
+
+    }
+
+    private function setRole($role, $person)
+    {
+        $res = 'child';
+        switch($role) {
+            // since differentiate between husband/wife and inlaw a manual adjustment is needed
+            case 'Spouse':
+            case 'Primary Contact':
+                $res = $person->gender == 'm' ? 'husband' : 'wife';
+                break;
+            case 'Sibling':
+                $res = $person->gender == 'm' ? 'brother' : 'sister';
+                break;
+            case 'Grandfather':
+                $res = 'grandfather';
+                break;
+            case 'Grandmother':
+                $res = 'grandmother';
+                break;
+            default:
+                $res = 'child';
+        }
+        return $res;
     }
 
     private function logImport($type, $remoteId, $object)
