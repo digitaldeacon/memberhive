aec37fa8fa49935899e994d931b79c2c07494341
axtho <thomas.hochstetter@me.com>
2018-02-23 20:30:47 +0100

    add git version script, upgrades

---
+++ apps/web/src/environments/version.ts
@@ -0,0 +1,25 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "v2.0.0-beta.1-62-g32f35f5-dirty",
+    "hash": "g32f35f5",
+    "distance": 62,
+    "tag": "v2.0.0-beta.1",
+    "semver": {
+        "raw": "v2.0.0-beta.1",
+        "major": 2,
+        "minor": 0,
+        "patch": 0,
+        "prerelease": [
+            "beta",
+            1
+        ],
+        "build": [],
+        "version": "2.0.0-beta.1"
+    },
+    "suffix": "62-g32f35f5-dirty",
+    "semverString": "2.0.0-beta.1+62.g32f35f5",
+    "version": "5.2.0"
+};
+/* tslint:enable */
