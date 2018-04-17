fc8ab3bdfd547a190021ec3bd94f4394b2bbe086
axtho <thomas.hochstetter@me.com>
2018-04-17 13:56:14 +0200

    #182 pipeable operators

--- libs/core/src/modules/auth/auth.effects.ts
+++ libs/core/src/modules/auth/auth.effects.ts
@@ -1,15 +1,10 @@
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/startWith";
-import "rxjs/add/operator/switchMap";
-import "rxjs/add/operator/exhaustMap";
-import "rxjs/add/operator/catch";
-import "rxjs/add/observable/of";
-import "rxjs/add/operator/take";
+import { catchError, map, tap, switchMap, exhaustMap } from "rxjs/operators";
+import { of } from "rxjs/observable/of";
 
 import { Injectable } from "@angular/core";
 import { HttpErrorResponse, HttpClient } from "@angular/common/http";
 import { Action } from "@ngrx/store";
-import { Effect, Actions } from "@ngrx/effects";
+import { Effect, Actions, ofType } from "@ngrx/effects";
 import { Router } from "@angular/router";
 import { Observable } from "rxjs/Observable";
 import { DataPersistence } from "@nrwl/nx";
@@ -23,60 +18,67 @@ import { AppState } from "../../store";
 @Injectable()
 export class AuthEffects {
   @Effect()
-  public login$: Observable<Action> = this._actions$
-    .ofType(actions.AUTHENTICATE)
-    .map((action: actions.AuthenticateAction) => action.payload)
-    .switchMap((credentials: Credentials) =>
-      this._http
-        .post<User>("/api/login/login", {
-          username: credentials.username,
-          password: credentials.password
-        })
-        .map((r: any) => {
-          if (typeof r === "string") {
-            return new actions.AuthenticationFailureAction(r);
-          }
-          this._authSrv.token = r.user.token;
-          this._authSrv.personId = r.user.personId;
-          this._authSrv.createdAt = new Date();
-          return new actions.AuthenticationSuccessAction(r.user);
-        })
-        .catch((response: HttpErrorResponse) => {
-          return Observable.of(
-            new actions.AuthenticationFailureAction(response)
-          );
-        })
-    );
+  public login$: Observable<Action> = this._actions$.pipe(
+      ofType(actions.AUTHENTICATE),
+      map((action: actions.AuthenticateAction) => action.payload),
+      switchMap((credentials: Credentials) =>
+          this._http
+              .post<User>("/api/login/login", {
+                  username: credentials.username,
+                  password: credentials.password
+              })
+              .pipe(
+                  map((r: any) => {
+                      if (typeof r === "string") {
+                          return new actions.AuthenticationFailureAction(r);
+                      }
+                      this._authSrv.token = r.user.token;
+                      this._authSrv.personId = r.user.personId;
+                      this._authSrv.createdAt = new Date();
+                      return new actions.AuthenticationSuccessAction(r.user);
+                  }),
+                  catchError((response: HttpErrorResponse) => {
+                      return Observable.of(
+                          new actions.AuthenticationFailureAction(response)
+                      );
+                  })
+              )
+      )
+  );
 
   @Effect()
-  public $reauth: Observable<Action> = this._actions$
-    .ofType(actions.REAUTHENTICATE)
-    .map((action: actions.ReAuthenticateAction) => action.payload)
-    .exhaustMap((token: string) => {
-      return this._http
-        .get("api/login/alive")
-        .map((r: any) => {
-          return new actions.ReAuthenticationSuccessAction({
-            token: this._authSrv.token,
-            personId: this._authSrv.personId
-          });
-        })
-        .do(() => this._router.navigate(["/dashboard"]))
-        .catch((response: HttpErrorResponse) => {
-          return Observable.of(
-            new actions.AuthenticationFailureAction(response)
-          );
-        })
-        .do(() => this._router.navigate(["/login"]));
-    });
+  public $reauth: Observable<Action> = this._actions$.pipe(
+      ofType(actions.REAUTHENTICATE),
+      map((action: actions.ReAuthenticateAction) => action.payload),
+      exhaustMap((token: string) => {
+          return this._http
+              .get("api/login/alive")
+              .pipe(
+                  map((r: any) => {
+                      return new actions.ReAuthenticationSuccessAction({
+                          token: this._authSrv.token,
+                          personId: this._authSrv.personId
+                      });
+                  }),
+                  tap(() => this._router.navigate(["/dashboard"])),
+                  catchError((response: HttpErrorResponse) => {
+                      return Observable.of(
+                          new actions.AuthenticationFailureAction(response)
+                      );
+                  }),
+                  tap(() => this._router.navigate(["/login"]))
+              )
+      })
+  );
 
   @Effect()
-  public signOut: Observable<Action> = this._actions$
-    .ofType(actions.SIGN_OUT)
-    .map(value => new actions.SignOutSuccessAction())
-    .do((v: any) => {
-      this._router.navigate(["/login"]);
-    });
+  public signOut: Observable<Action> = this._actions$.pipe(
+      ofType(actions.SIGN_OUT),
+      map(value => new actions.SignOutSuccessAction()),
+      tap((v: any) => {
+          this._router.navigate(["/login"]);
+      })
+  );
 
   constructor(
     private _s: DataPersistence<AppState>,
