96dbfb6d415b5ebca384431d2318be4da9b55257
axtho <thomas.hochstetter@me.com>
2018-04-25 19:50:13 +0200

    upgrades and fix lint error

--- apps/web/src/environments/version.ts
+++ apps/web/src/environments/version.ts
@@ -2,9 +2,9 @@
 /* tslint:disable */
 export const VERSION = {
   dirty: true,
-  raw: 'v2.0.0-beta3-19-g5200bf8-dirty',
-  hash: 'g5200bf8',
-  distance: 19,
+  raw: 'v2.0.0-beta3-23-g621cd26-dirty',
+  hash: 'g621cd26',
+  distance: 23,
   tag: 'v2.0.0-beta3',
   semver: {
     raw: 'v2.0.0-beta3',
@@ -15,8 +15,8 @@ export const VERSION = {
     build: [],
     version: '2.0.0-beta3'
   },
-  suffix: '19-g5200bf8-dirty',
-  semverString: '2.0.0-beta3+19.g5200bf8',
+  suffix: '23-g621cd26-dirty',
+  semverString: '2.0.0-beta3+23.g621cd26',
   version: '2.0.0-beta.1'
 };
 /* tslint:enable */
