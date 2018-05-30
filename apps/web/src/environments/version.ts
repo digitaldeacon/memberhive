f8e80cc4a8656b13b69f55bb2c3b9d7d1e3f1af8
axtho <thomas.hochstetter@me.com>
2018-05-30 09:45:39 +0200

    format and upgrades

--- apps/web/src/environments/version.ts
+++ apps/web/src/environments/version.ts
@@ -1,22 +1,24 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-  dirty: true,
-  raw: 'v2.0.0-beta3-24-g96dbfb6-dirty',
-  hash: 'g96dbfb6',
-  distance: 24,
-  tag: 'v2.0.0-beta3',
-  semver: {
-    raw: 'v2.0.0-beta3',
-    major: 2,
-    minor: 0,
-    patch: 0,
-    prerelease: ['beta3'],
-    build: [],
-    version: '2.0.0-beta3'
-  },
-  suffix: '24-g96dbfb6-dirty',
-  semverString: '2.0.0-beta3+24.g96dbfb6',
-  version: '2.0.0-beta.1'
+    "dirty": true,
+    "raw": "v2.0.0-beta3-31-gb643108-dirty",
+    "hash": "gb643108",
+    "distance": 31,
+    "tag": "v2.0.0-beta3",
+    "semver": {
+        "raw": "v2.0.0-beta3",
+        "major": 2,
+        "minor": 0,
+        "patch": 0,
+        "prerelease": [
+            "beta3"
+        ],
+        "build": [],
+        "version": "2.0.0-beta3"
+    },
+    "suffix": "31-gb643108-dirty",
+    "semverString": "2.0.0-beta3+31.gb643108",
+    "version": "2.0.0-beta.1"
 };
 /* tslint:enable */
